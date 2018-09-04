42c702534e4da75cf827ab6f4bae2d5dc0ddbec9
Bhargavi05 <42903586+Bhargavi05@users.noreply.github.com>
2018-09-05 02:54:44 +0530

    Added  excel utils to read test data from xls

--- ebayApp/src/main/java/com/ebay/Generic/Functions/ExcelDataProvider.java
+++ ebayApp/src/main/java/com/ebay/Generic/Functions/ExcelDataProvider.java
@@ -1,5 +1,271 @@
 package com.ebay.Generic.Functions;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Hashtable;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelDataProvider {
+
+	private static XSSFCell Cell;
+	String filename;
+	String SheetName;
+	public String path;
+	public FileInputStream fis = null;
+	public FileOutputStream fileOut = null;
+	private XSSFWorkbook workbook = null;
+	private XSSFSheet sheet = null;
+	private XSSFRow row = null;
+	private XSSFCell cell = null;
+	@SuppressWarnings("unused")
+	private static XSSFFormulaEvaluator evaluator;
+
+	public ExcelDataProvider(String path) {
+
+		this.path = path;
+		try {
+			fis = new FileInputStream(path);
+			workbook = new XSSFWorkbook(fis);
+			sheet = workbook.getSheetAt(0);
+			fis.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	// To read test data in class
+	public static Object[][] dataProvider(String fileName, String SheetName, String noMentionOfSkip) throws Exception {
+
+		ExcelDataProvider xls_file = new ExcelDataProvider(fileName);
+		int colCount = xls_file.getColumnCount(SheetName);
+		int rowCount = xls_file.getRowCount(SheetName);
+
+		int rowCountActual = xls_file.getRowCount(SheetName);
+
+		int rowReduce = 1;
+
+		if (noMentionOfSkip.equalsIgnoreCase("Y")) {
+
+			for (int row = 2; row <= rowCount; row++) {
+
+				if (xls_file.getCellData(SheetName, "execute", row).equalsIgnoreCase("N")) {
+
+					rowReduce++;
+				}
+
+			}
+
+		}
+
+		rowCountActual = rowCount - rowReduce;
+
+		Object[][] data = new Object[rowCountActual][1];
+
+		List<String> colNames = new ArrayList<String>();
+		int colIndex = 0;
+		// colNames.add(0, "Dummy");
+
+		for (int col = 0; col < colCount; col++) {
+
+			colNames.add(col, xls_file.getCellData(SheetName, col, 1));
+
+		}
+
+		int actualRowCount = 2;
+		for (int row = 2; row <= rowCount; row++) {
+
+			if (!xls_file.getCellData(SheetName, "execute", row).equalsIgnoreCase("N")
+					&& noMentionOfSkip.equalsIgnoreCase("Y")) {
+
+				Hashtable<String, String> table = new Hashtable<String, String>();
+				for (int col = 0; col < colCount; col++) {
+
+					table.put(colNames.get(col), xls_file.getCellData(SheetName, col, row));
+
+				}
+
+				data[actualRowCount - 2][0] = table;
+				actualRowCount++;
+			}
+
+			if (noMentionOfSkip.equalsIgnoreCase("N")) {
+
+				Hashtable<String, String> table = new Hashtable<String, String>();
+				for (int col = 0; col < colCount; col++) {
+
+					table.put(colNames.get(col), xls_file.getCellData(SheetName, col, row));
+
+				}
+
+				data[actualRowCount - 2][0] = table;
+				actualRowCount++;
+			}
+
+		}
+
+		return data;
+	}
+
+	// returns number of columns in a sheet
+	public int getColumnCount(String sheetName) {
+		// check if sheet exists
+		if (!isSheetExist(sheetName))
+			return -1;
+
+		sheet = workbook.getSheet(sheetName);
+		row = sheet.getRow(0);
+
+		if (row == null)
+			return -1;
+
+		return row.getLastCellNum();
+
+	}
+
+	// find whether sheets exists
+	public boolean isSheetExist(String sheetName) {
+		int index = workbook.getSheetIndex(sheetName);
+		if (index == -1) {
+			index = workbook.getSheetIndex(sheetName.toUpperCase());
+			if (index == -1)
+				return false;
+			else
+				return true;
+		} else
+			return true;
+	}
+
+	// TO get the row count
+	public int getRowCount(String sheetName) {
+		int index = workbook.getSheetIndex(sheetName);
+		if (index == -1)
+			return 0;
+		else {
+			sheet = workbook.getSheetAt(index);
+			int number = sheet.getLastRowNum() + 1;
+			return number;
+		}
+
+	}
+
+	// returns the data from a cell
+	public String getCellData(String sheetName, String colName, int rowNum) {
+		try {
+			if (rowNum <= 0)
+				return "";
+
+			int index = workbook.getSheetIndex(sheetName);
+			int col_Num = -1;
+			if (index == -1)
+				return "";
+
+			sheet = workbook.getSheetAt(index);
+			row = sheet.getRow(0);
+			for (int i = 0; i < row.getLastCellNum(); i++) {
+				// System.out.println(row.getCell(i).getStringCellValue().trim());
+				if (row.getCell(i).getStringCellValue().trim().equals(colName.trim()))
+					col_Num = i;
+			}
+			if (col_Num == -1)
+				return "";
+
+			sheet = workbook.getSheetAt(index);
+			row = sheet.getRow(rowNum - 1);
+			if (row == null)
+				return "";
+			cell = row.getCell(col_Num);
+
+			if (cell == null)
+				return "";
+			// System.out.println(cell.getCellType());
+			if (cell.getCellType() == Cell.CELL_TYPE_STRING)
+				return cell.getStringCellValue();
+			else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC || cell.getCellType() == Cell.CELL_TYPE_FORMULA) {
+
+				String cellText = String.valueOf(cell.getNumericCellValue());
+				if (HSSFDateUtil.isCellDateFormatted(cell)) {
+					// format in form of M/D/YY
+					double d = cell.getNumericCellValue();
+
+					Calendar cal = Calendar.getInstance();
+					cal.setTime(HSSFDateUtil.getJavaDate(d));
+					cellText = (String.valueOf(cal.get(Calendar.YEAR))).substring(2);
+					cellText = cal.get(Calendar.DAY_OF_MONTH) + "/" + cal.get(Calendar.MONTH) + 1 + "/" + cellText;
+
+					// System.out.println(cellText);
+
+				}
+
+				return cellText;
+			} else if (cell.getCellType() == Cell.CELL_TYPE_BLANK)
+				return "";
+			else
+				return String.valueOf(cell.getBooleanCellValue());
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			return "row " + rowNum + " or column " + colName + " does not exist in xls";
+		}
+	}
+
+	// returns the data from a cell
+	public String getCellData(String sheetName, int colNum, int rowNum) {
+		try {
+			if (rowNum <= 0)
+				return "";
+
+			int index = workbook.getSheetIndex(sheetName);
+
+			if (index == -1)
+				return "";
+
+			sheet = workbook.getSheetAt(index);
+			row = sheet.getRow(rowNum - 1);
+			if (row == null)
+				return "";
+			cell = row.getCell(colNum);
+			if (cell == null)
+				return "";
+
+			if (cell.getCellType() == Cell.CELL_TYPE_STRING)
+				return cell.getStringCellValue();
+			else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC || cell.getCellType() == Cell.CELL_TYPE_FORMULA) {
+
+				String cellText = String.valueOf(cell.getNumericCellValue());
+				if (HSSFDateUtil.isCellDateFormatted(cell)) {
+					// format in form of M/D/YY
+					double d = cell.getNumericCellValue();
+
+					Calendar cal = Calendar.getInstance();
+					cal.setTime(HSSFDateUtil.getJavaDate(d));
+					cellText = (String.valueOf(cal.get(Calendar.YEAR))).substring(2);
+					cellText = cal.get(Calendar.MONTH) + 1 + "/" + cal.get(Calendar.DAY_OF_MONTH) + "/" + cellText;
+
+					// System.out.println(cellText);
+
+				}
+
+				return cellText;
+			} else if (cell.getCellType() == Cell.CELL_TYPE_BLANK)
+				return "";
+			else
+				return String.valueOf(cell.getBooleanCellValue());
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			return "row " + rowNum + " or column " + colNum + " does not exist  in xls";
+		}
+	}
 
 }
